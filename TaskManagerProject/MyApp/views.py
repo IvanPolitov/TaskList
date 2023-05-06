ee9986a1890acb00b33236a02dc7dd7ac3da2888
ivanh <ivan.hanter21@mail.ru>
2023-05-06 11:08:17 +0500

    Закончил видеоурок. Продолжения нет, так что дальше сам. Сейчас есть главная страничка, о нас и создание таска. В создании таска заполняем форму и добавляем данные в бд. На главной отображаются все таски.

--- TaskManagerProject/MyApp/views.py
+++ TaskManagerProject/MyApp/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Task
+from .forms import TaskForm
 
 
 def index(request):
-    return render(request, 'MyApp/index.html')
+    tasks = Task.objects.order_by('-id')
+    return render(request, 'MyApp/index.html', {'title': 'Главная страница сайта', 'tasks': tasks})
 
 def about(request):
     return render(request, 'MyApp/about.html')
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'form iivalid and oguzok'
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'MyApp/create.html', context)
